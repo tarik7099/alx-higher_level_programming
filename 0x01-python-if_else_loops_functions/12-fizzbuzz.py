d78f7d69b3f67447adbdf611958297da04449fff
tarik khadri <tarikkhadri11@gmail.com>
2023-11-29 01:40:35 +0100

    12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(0,100):
-        if i % 3 == 0:
-            print("Fizz")
-        if i % 5 == 0:
-            print("Buzz")
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz ", end='')
+    elif i % 3 == 0:
+        print("Fizz ", end='')
+    elif i % 5 == 0:
+        print("Buzz ", end='')
+    else:
+        print("{:d} ".format(i), end='')
