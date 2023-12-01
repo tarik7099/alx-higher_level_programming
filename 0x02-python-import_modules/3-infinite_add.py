3a3c4d27f68c3675f53da79c53301199a8b6eead
tarik khadri <tarikkhadri11@gmail.com>
2023-12-01 03:31:28 +0100

    fix

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,14 +3,16 @@ if __name__ == "__main__":
     import sys
     len1 = len(sys.argv)
     number = 0
-
     for i in range(1, len1):
-        for j in range(i + 1, i + 2):
-            if i + 2 > len1:
-                break
-            if j >= 3:
-                number = number + int(sys.argv[j])
-            else:
-                number += int(sys.argv[i]) + int(sys.argv[j])
+        if len1 == 2:
+            break
+        else:
+            for j in range(i + 1, i + 2):
+                if i + 2 > len1:
+                    break
+                if j >= 3:
+                    number = number + int(sys.argv[j])
+                else:
+                    number += int(sys.argv[i]) + int(sys.argv[j])
             
-    print(number)
+        print(number)
