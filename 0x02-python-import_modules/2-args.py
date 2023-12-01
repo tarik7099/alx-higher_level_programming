bb4a464b34758ac734e053f1c8b99f9933462895
tarik khadri <tarikkhadri11@gmail.com>
2023-12-01 01:44:41 +0100

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,10 @@
 if __name__ == "__main__":
     import sys
     len1 = len(sys.argv)
-    print("{} arguments.".format(len1 - 1))
+    if len1 - 1== 0: 
+        print("{} arguments.".format(len1 - 1))
+    else:
+         print("{} arguments:".format(len1 - 1))
     for i in range(len1):
             i += + 1
             if i == len1:
