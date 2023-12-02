233feb7be73af69b9add615585a9343274351e80
tarik khadri <tarikkhadri11@gmail.com>
2023-12-02 15:19:48 +0100

    fix

--- 0x03-python-data_structures/2-main.py
+++ 0x03-python-data_structures/2-main.py
@@ -2,8 +2,10 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return None
-    my_list[idx] = element
-    return my_list
+    else:
+        my_list[idx] = element
+        return my_list
+
 #replace_in_list = __import__('2-replace_in_list').replace_in_list
 
 my_list = [1, 2, 3, 4, 5]
