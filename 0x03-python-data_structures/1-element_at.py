99d5262080c5e479a30808c199a814bd2a8de193
tarik khadri <tarikkhadri11@gmail.com>
2023-12-02 14:29:18 +0100

    fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    
-    if idx < 0 or (idx > len(my_list)- 1):
+    if idx < 0 or (idx > len(my_list) - 1):
         return None
-    
     for i in range(len(my_list)):
-        if  my_list[idx] == my_list[i]:
+        if my_list[idx] == my_list[i]:
             idxpass = my_list[i]
     return idxpass
