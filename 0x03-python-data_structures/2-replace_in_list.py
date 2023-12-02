fba0a1772e81bdb4845bc084ca635bc07764ff33
tarik khadri <tarikkhadri11@gmail.com>
2023-12-02 15:08:51 +0100

    fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+#my_list = [1, 2, 3, 4, 5]
+#idx = 3
+#new_element = 9
 def replace_in_list(my_list, idx, element):
+    new_list = my_list[:]
     if idx < 0 or idx > len(my_list) - 1:
         return None
-    my_list.insert(idx, element)
-    return my_list
+    new_list.insert(idx , element)
+    del new_list[idx + 1]
+    return new_list
+
+#if __name__=="__main__":
+#    hy =replace_in_list(my_list , idx , new_element)
+#    print(hy)
