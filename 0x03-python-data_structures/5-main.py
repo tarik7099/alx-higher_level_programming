58da53f7b274ebd8cb2350f02af0e71e764b19ff
tarik khadri <tarikkhadri11@gmail.com>
2023-12-03 03:27:18 +0100

    fix

---
+++ 0x03-python-data_structures/5-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    char_c = 'c'
+    char_C = 'C'
+    new_lis = []
+    my_string_new = ""
+    if my_string:
+        for i in range(0, len(my_string)):
+            new_lis.append(my_string[i])
+        # Remove all occurrences of 'c' from new_lis using list comprehension
+        new_lis = [char for char in new_lis if char != char_c and char_c!=char_C]
+        my_string_new = "".join(new_lis)
+        return my_string_new
+
+print(no_c("Best School"))  # Output: 'Best Shool'
+print(no_c("Chicago"))      # Output: 'hiago'
+print(no_c("C is fun!"))    # Output: ' is fun!'
