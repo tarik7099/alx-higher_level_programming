f1f5f393b22fed9fd7c255eea9fa93ea3b7e8d48
tarik khadri <tarikkhadri11@gmail.com>
2023-12-03 04:37:12 +0100

    fix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix and len(matrix) > 1:
+        for i in range(0, 3):
+            print("{} {} {}".format(matrix[i][0], matrix[i][1], matrix[i][2]))y
