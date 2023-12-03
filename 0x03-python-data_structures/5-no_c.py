87651a4241a9323a60ed394e8d6cf4edb94db420
tarik khadri <tarikkhadri11@gmail.com>
2023-12-03 03:33:41 +0100

    fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,11 +4,11 @@ def no_c(my_string):
     char_C = 'C'
     new_lis = []
     my_string_new = ""
-    if my_string:
-        for i in range(0, len(my_string)):
-            new_lis.append(my_string[i])
+    
+    for i in range(0, len(my_string)):
+        new_lis.append(my_string[i])
         # Remove all occurrences of 'c' from new_lis using list comprehension
-        new_lis = [char for char in new_lis if char != char_c and (char != char_C)]
+    new_lis = [char for char in new_lis if char != char_c and (char != char_C)]
         
-        my_string_new = "".join(new_lis)
-        return my_string_new
+    my_string_new = "".join(new_lis)
+    return my_string_new
