b87ff99c2aa3199e0fbadc0d44bee93f1144d68d
tarik khadri <tarikkhadri11@gmail.com>
2023-12-03 19:07:04 +0100

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length1 = len(sentence)
-    first_char = sentence[0]
-    return length1, first_char
+    length = len(sentence)
+    first = sentence[0]
+    return length, first
