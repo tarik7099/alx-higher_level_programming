0e10176f724a2f5c23bbe1c498c04aedf60e2c8f
tarik khadri <tarikkhadri11@gmail.com>
2023-12-03 04:50:37 +0100

    fix

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -2,7 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix and len(matrix) > 1:
         for i in range(0 ,3):
-            print("{} {} {}".format(matrix[i][0], matrix[i][1], matrix[i][2]))
+           print("{} {} {}".format(matrix[i][0], matrix[i][1], matrix[i][2]))
+
 
                 
             
