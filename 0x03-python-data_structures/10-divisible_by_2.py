ca0f25773c6f3cae87494f072ba69f17d0c57970
tarik khadri <tarikkhadri11@gmail.com>
2023-12-03 23:21:42 +0100

    fox

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,5 +2,5 @@
 def divisible_by_2(my_list=[]):
     myltiple_list = []
     for multiple in my_list:
-            myltiple_list.append(multiple % 2 == 0)
+        myltiple_list.append(multiple % 2 == 0)
     return myltiple_list
