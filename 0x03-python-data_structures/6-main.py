1a82c05afbd3aac3a15d2c64ca0caca9e4d1a884
tarik khadri <tarikkhadri11@gmail.com>
2023-12-03 05:12:14 +0100

    fix

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    
-    
-    if matrix and len(matrix) >= 3:
-        for i in range(3):
-            if len(matrix[i]) >= 3:
-                print("{} {} {}".format(matrix[i][0], matrix[i][1], matrix[i][2]))
-    else:
-        print()
-# Test cases
+print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
+
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     if matrix and len(matrix) >= 3:
         for i in range(3):
             if len(matrix[i]) >= 3:
-                print("{:d} {:d} {:D}".format(matrix[i][0], matrix[i][1], matrix[i][2]))
+                print("{:d} {:d} {:d}".format(matrix[i][0], matrix[i][1], matrix[i][2]))
     else:
         print()
