2a9689c6dc4167a56fa840b4d26f6f4eb14cc2f1
tarik khadri <tarikkhadri11@gmail.com>
2023-12-03 19:11:48 +0100

    fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        sentence[0] = None 
     length = len(sentence)
-    first = sentence[0]
+    if not sentence:
+        sentence[0] = None
+    else:
+        first = sentence[0]
     return length, first
