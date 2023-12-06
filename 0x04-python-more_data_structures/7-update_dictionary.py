e23eddaa4a933acd7278740b0415d0b415a6814a
tarik khadri <tarikkhadri11@gmail.com>
2023-12-06 23:09:22 +0100

    fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,8 +4,8 @@ def update_dictionary(a_dictionary, key, value):
         a_dictionary[key] = value
     else:
         for i in a_dictionary:
+            if i = key
             a_dictionary[i] = value
-
 
     return a_dictionary
     
