784bb08a93d47d661969bf295855832528dc0e76
tarik khadri <tarikkhadri11@gmail.com>
2023-12-06 17:03:04 +0100

    fxi

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-'''
 def square_matrix_simple(matrix=[]):
     list1 = []
-    for i in range(3):
-        new_list = []
-        for j in range(3):
-            value = matrix[i][j] ** 2
-            new_list.append(value)
-        list1.append(new_list)
-    return list1
-'''
-def square_matrix_simple(matrix=[]):
-    list1= []
     for new_matrix in matrix:
         new_list = []
         for new_matrix1 in new_matrix:
-            value = new_matrix1 **2
+            value = new_matrix1 ** 2
             new_list.append(value)
         list1.append(new_list)
     return(list1)
