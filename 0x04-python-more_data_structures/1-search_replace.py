ded8be25d497a396b1589b745d96c524ae35ef72
tarik khadri <tarikkhadri11@gmail.com>
2023-12-06 17:37:04 +0100

    new

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,5 +4,5 @@ def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     for i in range(len(new_list)):
         if new_list[i] == search:
-            new_list[i] = replac
+            new_list[i] = replace
     return new_list
