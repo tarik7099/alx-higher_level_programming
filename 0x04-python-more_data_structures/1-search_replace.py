0b897b365a19933d1620a164799181e3ee5c046f
tarik khadri <tarikkhadri11@gmail.com>
2023-12-06 17:26:51 +0100

    new

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    idx = my_list.index(search)
-    my_list.insert(idx, replace)
-    return my_list
+    new_list = []
+    for new_list1 in my_list:
+        new_list.append(new_list1)
+    idx = new_list.index(search)
+    new_list.insert(idx, replace)
+    del new_list[idx + 1]
+    return new_list
