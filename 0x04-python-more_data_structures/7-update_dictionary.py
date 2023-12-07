82df6daa67328b583d0ac52307b8f5414f63f01f
tarik khadri <tarikkhadri11@gmail.com>
2023-12-07 14:33:39 +0100

    fxi

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,6 @@
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary.keys():
         a_dictionary[key] = value
-    elif not key in a_dictionary.keys():
-        a_dictionary.update({key : value})
-
+    elif key not in a_dictionary.keys():
+        a_dictionary.update({key: value})
     return a_dictionary
