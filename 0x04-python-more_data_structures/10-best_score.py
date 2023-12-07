c38291fca53b1bda0cd9fa77de76533fbbd3958c
tarik khadri <tarikkhadri11@gmail.com>
2023-12-07 15:18:36 +0100

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     list1 = []
+    list2 = []
     if not a_dictionary:
         return None
     for key, value in a_dictionary.items():
         list1.append(value)
+        list2.append(key)
         for j in range(len(list1)):
             for i in range(len(list1) - 1):
                 if list1[j] > list1[i + 1]:
-                    key = list1[j]
+                    key = list2[j]
     return key
