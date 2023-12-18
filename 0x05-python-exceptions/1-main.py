c92c53cfd57609133c6b9a7b69d14e6fda6cc5ae
tarik khadri <tarikkhadri11@gmail.com>
2023-12-18 15:29:52 -0800

    task 3

--- 0x05-python-exceptions/1-main.py
+++ 0x05-python-exceptions/1-main.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        if value == "{:d}".format(value):
-            return True
-        else:
-            return False
-            #print("%d".format(value), end='')
-        
-    except Exception as e:
-        pass
-        
-
-#safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
+safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
 value = 89
 has_been_print = safe_print_integer(value)
@@ -27,4 +15,3 @@ value = "School"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
-
