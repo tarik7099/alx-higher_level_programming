9c8e2013c9d0a11edc0c0f89393698315f8861c8
tarik khadri <tarikkhadri11@gmail.com>
2023-12-19 19:14:13 -0800

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,8 +4,9 @@
 
 class Square:
     """class square"""
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0,0)):
         self.__size = size
+        self.position = position
 
     @property
     def size(self):
