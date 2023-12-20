c2f0098d1635d8965fee8f1338b21750688d0370
tarik khadri <tarikkhadri11@gmail.com>
2023-12-19 18:50:15 -0800

    fix 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" class square """
+
+
+class Square:
+    """class square"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, size=0):
+        if type(size) is int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
