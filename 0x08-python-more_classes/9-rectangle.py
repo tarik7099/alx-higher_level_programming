882fc1612f03b5baae8c45ab05e2e21704bf78c7
tarik khadri <tarikkhadri11@gmail.com>
2024-01-05 22:59:06 +0100

    we donc

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -73,9 +73,7 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        ara1 = rect_1.__height * rect_1.__width
-        ara2 = rect_2.__width * rect_2.__height
-        if (ara1 >= ara2):
+        if (rect_1.area() >= rect_2.area()):
             return rect_1
         else:
             rect_2
