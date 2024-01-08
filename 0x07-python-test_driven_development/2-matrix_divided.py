aed7e80d4ed55df31c6d75490fd1ca1a2dab9c2d
tarik khadri <tarikkhadri11@gmail.com>
2024-01-08 22:29:49 +0100

    fix1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,23 +4,24 @@
 This module provides a function to add two numbers and return their sum.
 """
 
+
 def matrix_divided(matrix, div):
     """
-    Divides each element in a matrix by a given number.
+    matrix two divided.
 
     Args:
     matrix (list of lists): The matrix to be divided.
     div (int or float): The number to divide each element in the matrix by.
 
     Returns:
-    list of lists: A new matrix with the divided elements.
+    list of lists: A new matrix.
 
     Raises:
     TypeError: If matrix is not a list of lists containing integers or floats.
     ValueError: If the divisor is not a number or if it's zero.
     """
     if not isinstance(div, (int, float)):
-        TypeError("div must be a number")
+        raise TypeError("div must be a number")
 
     n_m = [[], []]
     for i in range(len(matrix)):
