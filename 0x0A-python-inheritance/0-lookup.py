f42956412a9fa27004c572be51da3a57887bf1cb
tarik khadri <tarikkhadri11@gmail.com>
2024-01-09 02:37:00 +0100

    fix

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Write a function that returns the list of available attributes and methods"""
+
+def lookup(obj):
+    """
+    Returns a list of available attributes
+    Args:
+    obj: An object to inspect.
+    """
+    list = []
+    list.append(dir(obj))
+    return list
