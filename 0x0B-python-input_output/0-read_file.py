b12b63830b00a01b1e0239794d42d53e5035bb78
tarik khadri <tarikkhadri11@gmail.com>
2024-01-10 23:28:29 +0100

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 """ read file """
 
+
 def read_file(filename=""):
-    with open(filename, encoding="UTF-8") as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         print(file.read())
