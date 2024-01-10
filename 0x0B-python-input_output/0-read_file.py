81a7453cc29d52fdbca1bb45fec3e0c0b4883c3e
tarik khadri <tarikkhadri11@gmail.com>
2024-01-10 23:30:11 +0100

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,14 @@
 
 
 def read_file(filename=""):
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        no
+
+    """
     with open(filename, 'r', encoding="utf-8") as file:
         print(file.read())
