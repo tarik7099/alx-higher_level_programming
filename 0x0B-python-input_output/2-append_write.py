793e0f4b72deeb7e583a9953495ccdeebfe2ec2a
tarik khadri <tarikkhadri11@gmail.com>
2024-01-10 23:52:40 +0100

    fix

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+def append_write(filename="", text=""):
+    """
+    Append a string to the end
+
+    Parameters:
+    - filename (str): The name
+    - text (str): The string
+    Returns:
+    int: The number of characters
+    """
+    with open(filename, 'a', encoding='utf-8') as file:
+        characters_added = file.write(text)
+        return characters_added
