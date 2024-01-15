38db7cbc654e04077f867bb67d7e7c63456805b9
tarik khadri <tarikkhadri11@gmail.com>
2024-01-16 00:16:26 +0100

    fix task file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,25 +1,53 @@
 #!/usr/bin/python3
 
-"""This is a rectangle base"""
+"""This module defines the Square class."""
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
+    """Square class, inherits from Rectangle."""
+
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialize a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+            x (int): The x-coordinate of the square.
+            y (int): The y-coordinate of the square.
+            id (int): The ID of the square.
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """
+        Return a string representation of the Square.
+
+        Returns:
+            str: String representation of the Square.
+        """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
+        """Get the size of the square."""
         return self.width
-    
+
     @size.setter
     def size(self, value):
-        self.width =  value
+        """Set the size of the square."""
+        self.width = value
         self.height = value
+
     def update(self, *args, **kwargs):
+        """
+        Update the attributes of the square.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         attributs = ["id", "width", "height", "x", "y"]
         if args:
             for x in range(0, len(args)):
@@ -27,4 +55,3 @@ class Square(Rectangle):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-    
