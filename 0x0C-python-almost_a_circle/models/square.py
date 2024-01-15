c9f6941e452f01e37eb05d4aa58510208a0c4ff7
tarik khadri <tarikkhadri11@gmail.com>
2024-01-16 00:30:35 +0100

    fix task file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -55,3 +55,17 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        Return the dictionary representation of the Square.
+
+        Returns:
+            dict: Dictionary representation of the Square.
+        """
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
