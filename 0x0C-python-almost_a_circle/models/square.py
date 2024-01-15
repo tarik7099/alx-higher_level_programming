522c0056b4dcedf2f2abe7d654c4394bb1066a9b
tarik khadri <tarikkhadri11@gmail.com>
2024-01-16 00:22:42 +0100

    fix task file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -40,22 +40,17 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
-    def update(self, *args, **kwargs):
-        """
-        Update the attributes of the square.
-
-        Args:
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
-        """
-        attributs = ["id", "width", "height", "x", "y"]
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__setattr__(key, val)
-            return
-        elif args:
-            for x in range(0, len(args)):
-                setattr(self, attributs[x], args[x])
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+def update(self, *args, **kwargs):
+    '''
+        Updates the arguments in the class
+    '''
+    if len(args) == 0:
+        for key, val in kwargs.items():
+            self.__setattr__(key, val)
+        return
+    
+    self.id = args[0]
+    self.width = args[1]
+    self.height = args[2]
+    self.x = args[3]
+    self.y = args[4]
