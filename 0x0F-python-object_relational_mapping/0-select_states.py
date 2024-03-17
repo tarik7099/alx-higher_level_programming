531fc93496c4284efd0e0503afb4b6ee0de22e8d
tarikkhadri <tarikkhadri11@gmail.com>
2024-03-17 00:16:27 +0000

    da

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """  lists all states from the database hbtn_0e_0_usa """
+
 import MySQLdb
 import sys
-
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
@@ -16,4 +15,3 @@ if __name__ == "__main__":
         print(state)
     cur.close()
     db.close()
-    
