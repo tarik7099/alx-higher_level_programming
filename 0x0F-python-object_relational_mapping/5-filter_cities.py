99df91d1d82e709f07bf3908bb5cb8061c91f559
tarik khadri <tarikkhadri11@gmail.com>
2024-03-17 12:34:06 -0700

    fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,16 +3,15 @@
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    db_cursor = db.cursor()
-    db_cursor.execute("""SELECT cities.name FROM
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
                 cities INNER JOIN states ON states.id=cities.state_id
                 WHERE states.name=%s""", (sys.argv[4],))
-    rows_selected = db_cursor.fetchall()
-    if rows_selected is not None:
-        print(", ".join([row[1] for row in rows_selected]))
-    db_cursor.close()
+    rows = cur.fetchall()
+    tmp1 = list(row[0] for row in rows)
+    print(*tmp1, sep=", ")
+    cur.close()
     db.close()
