42fd8842bbd41724265b5efe1d45fa9b1ea6f4e1
tarik khadri <tarikkhadri11@gmail.com>
2024-04-23 23:15:32 +0100

    fd

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Status code:', response.statusCode);
+    return;
+  }
+
+  try {
+    const results = JSON.parse(body).results;
+    const count = results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0);
+    console.log(count);
+  } catch (parseError) {
+    console.error('Parsing error:', parseError);
+  }
+});
