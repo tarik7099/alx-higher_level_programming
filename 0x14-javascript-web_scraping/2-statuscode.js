8e8139078d04e22e06722e041ed9ee94ce92a098
tarik khadri <tarikkhadri11@gmail.com>
2024-04-23 23:14:29 +0100

    fd

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,10 @@
-#!/usr/bin/env node
-
+#!/usr/bin/node
 const request = require('request');
-
 const url = process.argv[2];
-
-request(url, (error, response) => {
-  if (error) {
-    console.error(error);
-    return;
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
   }
-  
-  console.log(`code: ${response.statusCode}`);
 });
-
