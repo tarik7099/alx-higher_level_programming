13ff408d36777abd8cdc819f83b52847c43726a3
tarik khadri <tarikkhadri11@gmail.com>
2024-04-23 23:17:52 +0100

    fd

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,30 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const url = process.argv[2];
-
-request(url, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Status code:', response.statusCode);
-    return;
-  }
-
-  try {
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
     const results = JSON.parse(body).results;
-    const count = results.reduce((count, movie) => {
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
-    }, 0);
-    console.log(count);
-  } catch (parseError) {
-    console.error('Parsing error:', parseError);
+    }, 0));
   }
 });
-
