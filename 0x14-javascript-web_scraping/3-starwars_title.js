cea0ead7e21d43791c30ee7e8888b9d1be638e49
tarik khadri <tarikkhadri11@gmail.com>
2024-04-23 23:16:30 +0100

    fd

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,29 +1,12 @@
 #!/usr/bin/node
 
-const request = require('request');
-
-const url = process.argv[2];
-
-request(url, function (error, response, body) {
+const req = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const id = process.argv[2];
+req.get(url + id, function (error, res, body) {
   if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Status code:', response.statusCode);
-    return;
-  }
-
-  try {
-    const results = JSON.parse(body).results;
-    const count = results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
-        ? count + 1
-        : count;
-    }, 0);
-    console.log(count);
-  } catch (parseError) {
-    console.error('Parsing error:', parseError);
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
   }
 });
